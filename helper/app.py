eb2aed7d50ee2603b94ed544ac3f097a46b14f0b
Eirini Vandorou <evandorou@cognity.gr>
2019-09-06 12:49:43 +0300

    db

--- helper/app.py
+++ helper/app.py
@@ -11,8 +11,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY") or 'dev_key',
-        SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL"),
-        SQLALCHEMY_TRACK_MODIFICATIONS=True,
+        DATABASE=os.environ.get("DATABASE_URL"),
     )
     # ensure the instance folder exists
     try:
