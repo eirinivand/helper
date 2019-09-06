507144081632db79c06447362018b41bff4188e0
Eirini Vandorou <evandorou@cognity.gr>
2019-09-06 12:35:49 +0300

    small changes to try running on heroku

---
+++ helper/app.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY") or 'dev_key',
+        SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=True,
+    )
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
+    from helper.models import db
+    db.init_app(app)
+    from helper import auth
+    app.register_blueprint(auth.bp)
+    from helper import validate
+    app.register_blueprint(validate.bp)
+    app.add_url_rule('/', endpoint='csvvalidator')
+
+    return app
