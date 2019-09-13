fc913d249c637fac41d80f4f6767fa42d9d5ee73
Eirini Vandorou <evandorou@cognity.gr>
2019-09-13 11:04:12 +0300

    removed not needed files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn helper:'create_app()'
--- helper/open_file.py
+++
@@ -1,6 +0,0 @@
-def open_file(file_to_open):
-    try:
-        my_file = open(file_to_open, "r")
-        return print(my_file.read())
-    except Exception as ex:
-        return print(ex)
